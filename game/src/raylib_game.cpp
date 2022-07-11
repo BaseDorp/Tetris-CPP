8f176083258ef64a73187770e72e56cc638dfe16
Sam Hirsch <samhirsch.dev@gmail.com>
2022-07-11 01:08:49 -0500

    falling cube and grid lines

--- game/src/raylib_game.cpp
+++ game/src/raylib_game.cpp
@@ -7,12 +7,23 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 500;  // Must be divisible by colums and gridSize
+    const int screenHeight = 1000; // must be divisble with rows and gridSize
+    const int rows = 10;
+    const int colums = 5; // TODO dont know why this is half and not 20x10
+    const int gridSize = screenWidth / rows;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
+    InitWindow(screenWidth, screenHeight, "Tetris");
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+
+    float fallSpeed = 5.0f;
+    int frameCounter = 0;
+    struct Square
+    {
+        int size = 50;
+        Vector2 position = { 0, (float)(screenWidth / 2 - size) };
+    };
+    Square* s = new Square();
     //--------------------------------------------------------------------------------------
 
     // Main game loop
@@ -20,23 +31,39 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        frameCounter++;
+
+        if (((frameCounter / 60) % 2) == 1)
+        {
+            s->position.y += gridSize;
+            frameCounter = 0;
+        }
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
+        
+        DrawRectangle(screenWidth / 2 + s->size, s->position.y, s->size, s->size, GREEN);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+        // Draw board
+        for (int i = gridSize; i < screenHeight; i += gridSize)
+        {
+            DrawLine(0, i, screenWidth, i, LIGHTGRAY);
+        }
+        for (int i = gridSize; i < screenWidth; i += gridSize)
+        {
+            DrawLine(i, 0, i, screenHeight, LIGHTGRAY);
+        }
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
+    delete s;
+    
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
